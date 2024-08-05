5b7f44fb6e18b98cbc83519fac49f66f455e124b
Jumpy85Plays <gabriel42498@gmail.com>
2024-08-05 13:37:49 +0800

    reverting update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import './custom.css'; // Correct path to Custom.css
-import './styles/Home.css'; // Correct path to Home.css
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <Router>
+      <NavigationBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
