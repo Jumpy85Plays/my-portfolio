47c6e0134dff41a614a3561abe94a5b1ec6b23ac
Jumpy85Plays <gabriel42498@gmail.com>
2024-08-05 13:32:26 +0800

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { Container, Row, Col, Card } from 'react-bootstrap';
-import personalImage from '../images/personal.jpg';
+import { Container, Row, Col } from 'react-bootstrap';
+import personalImage from '../images/personal.jpg'; // Adjust the path as needed
 import '../styles/Home.css'; // Import the CSS file
 
 const Home = () => {
   return (
-    <Container className="mt-4">
+    <Container className="home-container mt-5">
       <Row className="align-items-center">
-        <Col md={6} className="text-center text-md-left">
-          <img src={personalImage} alt="Personal" className="personal-image" />
+        <Col md={6}>
+          <img src={personalImage} alt="Gabriel F. Aguilar" className="personal-image" />
         </Col>
-        <Col md={6} className="text-center text-md-left">
-          <h1>Welcome to My Portfolio</h1>
+        <Col md={6}>
+          <h1>Hello! I'm Gabriel F. Aguilar</h1>
           <p>
-            Hello! I'm Gabriel F. Aguilar, a 3rd Year Student currently earning my Degree in Bachelor of Science and Technology. This is my personal portfolio where I showcase my skills and projects.
+            A 3rd Year Student currently earning my Degree in Bachelor of Science and Technology. 
+            This is my personal portfolio where I showcase my skills and projects.
           </p>
         </Col>
       </Row>
@@ -22,3 +23,4 @@ const Home = () => {
 };
 
 export default Home;
+
